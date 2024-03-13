c4d5964c88e8a4f18fcce99501745372b23bd839
Mullins182 <Mullins182@t-online.de>
2024-03-13 21:23:16 +0100

    button mouse enter and leave event method integrated, click effect border blinking for buttons implemented.

--- GewaechshausMitAnimation/MainWindow.xaml.cs
+++ GewaechshausMitAnimation/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using GewaechshausMitAnimation.Model;
 using System.Windows;
+using System.Windows.Media;
 
 namespace GewaechshausMitAnimation
 {
@@ -33,30 +34,92 @@ namespace GewaechshausMitAnimation
             setIndoorTemp.Text  = Convert.ToString(indoorTempSlider.Value);
         }
 
-        private void set_outdoor_temp_Click(object sender, RoutedEventArgs e)
+        private async void set_outdoor_temp_Click(object sender, RoutedEventArgs e)
         {
-            outdoorTemp                 = Convert.ToInt32(outdoorTempSlider.Value);
-            ActualTempOutdoor.Text      = $"{Convert.ToString(outdoorTemp)} °C";
+            if((double)outdoorTemp == outdoorTempSlider.Value)
+            {
 
-            Tomate.setSystems(indoorTemp, outdoorTemp);
+            }
+            else
+            {
+                outdoorTemp                 = Convert.ToInt32(outdoorTempSlider.Value);
+                ActualTempOutdoor.Text      = $"{Convert.ToString(outdoorTemp)} °C";
 
-            LueftungStatus.Text         = Tomate.getLueftungStatus();
-            KuehlungStatus.Text         = Tomate.getKuehlungStatus();
-            HeizungStatus.Text          = Tomate.getHeizungStatus();
-            VerdunklungStatus.Text      = Tomate.getVerdunklStatus();
+                Tomate.setSystems(indoorTemp, outdoorTemp);
+
+                LueftungStatus.Text         = Tomate.getLueftungStatus();
+                KuehlungStatus.Text         = Tomate.getKuehlungStatus();
+                HeizungStatus.Text          = Tomate.getHeizungStatus();
+                VerdunklungStatus.Text      = Tomate.getVerdunklStatus();
+
+                set_outdoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3E, 0x11, 0x11));
+
+                for (int i = 4; i > 0; i--)
+                {
+                    set_outdoor_temp.BorderThickness    = new Thickness(0,2.6,0,2.6);
+                    set_outdoor_temp.BorderBrush        = Brushes.LimeGreen;
+
+                    await Task.Delay(100);
+
+                    set_outdoor_temp.BorderThickness    = new Thickness(0);
+
+                    await Task.Delay(100);
+                }
+            }
         }
 
-        private void set_indoor_temp_Click(object sender, RoutedEventArgs e)
+        private async void set_indoor_temp_Click(object sender, RoutedEventArgs e)
         {
-            indoorTemp                  = Convert.ToInt32(indoorTempSlider.Value);
-            ActualTempIndoor.Text       = $"{Convert.ToString(indoorTemp)} °C";
+            if((double)indoorTemp == indoorTempSlider.Value)
+            {
 
-            Tomate.setSystems(indoorTemp, outdoorTemp);
+            }
+            else
+            {
+                indoorTemp                  = Convert.ToInt32(indoorTempSlider.Value);
+                ActualTempIndoor.Text       = $"{Convert.ToString(indoorTemp)} °C";
 
-            LueftungStatus.Text         = Tomate.getLueftungStatus();
-            KuehlungStatus.Text         = Tomate.getKuehlungStatus();
-            HeizungStatus.Text          = Tomate.getHeizungStatus();
-            VerdunklungStatus.Text      = Tomate.getVerdunklStatus();
+                Tomate.setSystems(indoorTemp, outdoorTemp);
+
+                LueftungStatus.Text         = Tomate.getLueftungStatus();
+                KuehlungStatus.Text         = Tomate.getKuehlungStatus();
+                HeizungStatus.Text          = Tomate.getHeizungStatus();
+                VerdunklungStatus.Text      = Tomate.getVerdunklStatus();
+
+                set_indoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3E, 0x11, 0x11));
+
+                for (int i = 4; i > 0; i--)
+                {
+                    set_indoor_temp.BorderThickness = new Thickness(0, 2.6, 0, 2.6);
+                    set_indoor_temp.BorderBrush = Brushes.LimeGreen;
+
+                    await Task.Delay(100);
+
+                    set_indoor_temp.BorderThickness = new Thickness(0);
+
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private void set_outdoor_temp_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            set_outdoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xAA, 0x10, 0x5A, 0x11));
+        }
+
+        private void set_outdoor_temp_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            set_outdoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3E, 0x11, 0x11));
+        }
+
+        private void set_indoor_temp_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            set_indoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xAA, 0x10, 0x5A, 0x11));
+        }
+
+        private void set_indoor_temp_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            set_indoor_temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3E, 0x11, 0x11));
         }
 
         //public static void SystemStatusOutput(GewaechshausSystems Tomate, int indoorTemp, int outdoorTemp)
